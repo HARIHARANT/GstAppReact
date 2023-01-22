83645ef6b5a78c17d7af64abdfabd54ca4d2a54c
HARIHARANT <30747099+HARIHARANT@users.noreply.github.com>
2023-01-22 15:54:15 +0530

    3 files modified

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './style.css';
-import Login from './src/component/Login';
+import Login from './src/pages/Login';
 import Profile from './src/component/Profile';
 
 export default function App() {
